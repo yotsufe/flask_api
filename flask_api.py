d96190e6c6a26c1d3b7aa1357eec6e5533408b0d
yotsufe <yotsufe@gmail.com>
2021-01-06 18:45:33 +0900

    ユーザー情報API追加

--- flask_api.py
+++ flask_api.py
@@ -2,12 +2,25 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+@app.route('/test', methods=['GET', 'POST'])
+def return_result():
+    return jsonify(result)
+
+
 result = {'title': 'pythonTEST', 'result': 200}
 
 
-@app.route('/test', methods=['GET', 'POST'])
-def post_json():
-    return jsonify(result)
+@app.route('/users', methods=['GET', 'POST'])
+def return_users():
+    return jsonify(users_json)
+
+
+users_json = [
+    {'id': 1, 'name': "Michel", "password": "asdf"},
+    {'id': 2, 'name': "Bob",    "password": "qwer"},
+    {'id': 3, 'name': "Jack",   "password": "zxcv"},
+]
 
 
 if __name__ == "__main__":
